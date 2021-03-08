4ca8365814e043f89f3253b4349f3aa5b6bbf22a
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-03-08 15:36:29 +0300

    letsencrypt

--- env/docker/letsencrypt/init-letsencrypt.sh
+++ env/docker/letsencrypt/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(smartbots.click www.smartbots.click)
 rsa_key_size=4096
-data_path="../../../../data/certbot"
+data_path="../../../data/certbot"
 email="gadjievsaid.1995@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
